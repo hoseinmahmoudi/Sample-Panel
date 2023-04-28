29d3bddd99717028aabeffb34343761281bdf3d8
hoseinmahmoudi <55023775+hoseinmahmoudi@users.noreply.github.com>
2023-04-28 16:35:02 +0330

    add persian lang

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import NestedRouteWrapper from "./routes/NestedRouteWrapper";
+import { useTranslation } from "react-i18next";
 
 function App() {
-  return <NestedRouteWrapper />;
+  const { t, i18n } = useTranslation();
+  console.log(i18n.language);
+  var currentLang = i18n.language;
+  return (
+    <div className={currentLang.match("fa") ? "app_rtl" : ""}>
+      <NestedRouteWrapper />;
+    </div>
+  );
 }
 
 export default App;
